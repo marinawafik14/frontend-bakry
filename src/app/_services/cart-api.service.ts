9afed86a14d24a12caff5a5e2b218aea96689ece
Eman Sameh <emansameh143@gmail.com>
2025-02-07 21:37:31 +0200

    ...

--- src/app/_services/cart-api.service.ts
+++ src/app/_services/cart-api.service.ts
@@ -1,20 +1,25 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from '../../environments/environment.development';
+import { AuthService } from '../_service/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartApiService {
-
-
   private apiUrl = 'http://localhost:8000/cart/add';
 
-  constructor(public httpClient: HttpClient) { }
+  header: any
+  constructor(public httpClient: HttpClient, _authService:AuthService) {
+      this.header = _authService.setHeaders()
+  }
+
 
   getCartForUser(userId:string):Observable<any>{
-      return this.httpClient.get<any>(`${environment.BASE_URL}/api/cart/user/${userId}`)
+      return this.httpClient.get<any>(`${environment.BASE_URL}/api/cart/user/${userId}`,
+        {headers: this.header}
+      )
   }
 
   removeCartItem(userId:string, productId:string):Observable<any>{
@@ -36,31 +41,15 @@ export class CartApiService {
   clearCart(userId:string):Observable<any>{
     return this.httpClient.delete(`${environment.BASE_URL}/api/cart/clear/${userId}`)
   }
-
-// ------------------------------------------------
-
     private cartItems: any[] = [];
     private cartCount = new BehaviorSubject<number>(0);
 
     cartCount$ = this.cartCount.asObservable(); // Expose count as Observable
 
-    // constructor() { }
-
     // Get cart items
     getCartItems() {
       return this.cartItems;
     }
-  
-    // Add to cart
-    // addToCart(product: any) {
-    //   if (product.stock > 0) {
-    //     this.cartItems.push(product);
-    //     this.cartCount.next(this.cartItems.length); // Update cart count
-    //     console.log("Product added to cart:", product);
-    //   } else {
-    //     alert("This product is out of stock!");
-    //   }
-    // }
   
     // Get current cart count
     getCartCount() {
@@ -68,22 +57,39 @@ export class CartApiService {
     }
 
   
-    addToCart(productId: string, quantity: number, price: number) {
-      return this.httpClient.post<any>(this.apiUrl, { productId, quantity, price }).subscribe(response => {
-        if (response.token) {
-          localStorage.setItem('token', response.token); // Update token in localStorage
-        }
-        this.cartCount.next(response.cartItems.length); // Update cart count
-      });
+  // Function to retrieve the token from SESSION STORAGE
+  private getAuthHeaders(): HttpHeaders {
+    const token = sessionStorage.getItem('tokenkey');
+    if (!token) {
+      console.error("No token found in SessionStorage");
     }
-  
+
+    return new HttpHeaders({
+      'Authorization': token ? `Bearer ${token}` : '',
+      'Content-Type': 'application/json'
+    });
+  }
+
+  // Updated Add Product to Cart
+  addToCart(productId: string, quantity: number, price: number): Observable<any> {
+    const requestBody = { productId, quantity, price };
+
+    // console.log("📤 Sending request to:", this.apiUrl);
+    // console.log("📦 Request Body:", requestBody);
+    // console.log("📝 Headers:", this.getAuthHeaders().keys());
+
+    return this.httpClient.post<any>(this.apiUrl, requestBody, {
+      headers: this.getAuthHeaders()
+    });
+  }
+
+  //Load Cart Count from Token
     loadCartCount() {
       const token = localStorage.getItem('token');
       if (token) {
-        const decodedToken: any = JSON.parse(atob(token.split('.')[1])); // Decode token
+      const decodedToken: any = JSON.parse(atob(token.split('.')[1]));
         this.cartCount.next(decodedToken.cartItems?.length || 0);
       }
     }
-
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 import { CartApiService } from '../_services/cart-api.service';
 
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
